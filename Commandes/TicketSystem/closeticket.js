9845260b1f1268da43c70184d86ad8d55442326c
Resoki <remyaubert71@gmail.com>
2022-04-04 21:28:34 +0200

    make it better

--- Commandes/TicketSystem/closeticket.js
+++ Commandes/TicketSystem/closeticket.js
@@ -26,7 +26,7 @@ module.exports = {
       })
       let channelLog = message.guild.channels.cache.find(ch => ch.id.startsWith(global.channelLog));
 
-      const embedLog = new Discord.MessageEmbed().setDescription(`${message.author.username} a **fermé** son ticket`).setColor('RED').setImage(message.author.displayAvatarURL({dynamic: true}))
+      const embedLog = new Discord.MessageEmbed().setDescription(`${message.author.username} a **fermé** son ticket`).setColor('RED')
       channelLog.send({embeds: [embedLog]})
 
     } 
