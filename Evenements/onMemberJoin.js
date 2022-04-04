b8f4da5be1fc1705929540631b9a3f9dbbaa253a
Resoki <remyaubert71@gmail.com>
2022-04-04 20:38:19 +0200

    make it better

--- Evenements/onMemberJoin.js
+++ Evenements/onMemberJoin.js
@@ -5,18 +5,18 @@ module.exports = {
 	name: 'guildMemberAdd',
 	once: false,
 	execute(member, bot) {
-    try {
-        var memberCount = member.guild.memberCount
-        console.log(member.user.username)
-        
-        const channel = bot.channels.cache.find(channel => channel.id === global.channelJoin);
-   
-        let welcomeEmbed = new MessageEmbed().setTitle(`Nouveau membre`).setDescription(`${member.user.username} vient de rejoindre le serveur !\nTotal membre: ${memberCount} `).setColor('RANDOM')
-        channel.send({embeds: [welcomeEmbed]})
-        
-    } 
-    catch(err) {
-        console.log('Une erreur a eu lieu: ', err);
-    }
+        try {
+            const channel = member.guild.channels.cache.find(channel => channel.id === global.channelJoin);
+            const guild = bot.guilds.cache.get(global.guild_id);
+            const userCount = guild.memberCount;
+            let welcomeEmbed = new MessageEmbed().setTitle(`Ho ! Un nouveau membre  !`)
+            .setDescription(`✨ Bienvenue **${member.user.username}** ✨\n sur le serveur **Voltera !**\n Tu es le **${userCount}** ième !`).setColor('RANDOM')
+            .setImage(member.user.displayAvatarURL({dynamic: true}))
+            .setTimestamp()
+            return channel.send({embeds: [welcomeEmbed]});
+        } 
+        catch(err) {
+            return channel.send('Une erreur a eu lieu: ', err);
+        }
 	},
 }
