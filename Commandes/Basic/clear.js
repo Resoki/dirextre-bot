8a578108b1460f8afc32dd6bc1d1f15468447169
Remy.Aubert <r.aubert@ldlc.com>
2022-04-05 16:45:27 +0200

    rev

--- Commandes/Basic/clear.js
+++ Commandes/Basic/clear.js
@@ -1,11 +1,24 @@
-const { MessageEmbed } = require('discord.js');
-const axios = require('axios')
 
 module.exports = {
     name: 'clear',
-    description: 'random dog image',
+    description: 'clear message',
     run: async(client, message, args) => {
-      if(!args[1]) message.reply('Ecrivez le nombre a delete')
+      var amount = parseInt(args[0])
+      const permission = message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS);
+      
+      if (!permission)
+        return message.reply(`❌ | Tu n'as pas la permission d'utiliser cette commande !`);
+
+        if (!amount) return message.channel.send(`Merci d'indiquer le nombre de messages à delete`)
+        if (amount > 100 || amount < 1) return message.channel.send("Merci de choisir un nombre entre 1 et 100")
+
+        message.channel.bulkDelete(amount).catch(err => {
+              message.channel.send(':x: A cause de la limitation Discord, je ne peux pas supprimer les messages de plus de 14 jours') })
+
+        let msg = await message.channel.send(`${amount} messages ont été supprimés !`)
+        setTimeout(() => {
+            msg.delete()
+        }, 2000)
 
     }
 }
